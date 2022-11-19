c2836376a83cdbf9317c76b2fabb641c839a1374
teromonte8 <tar.monteiro@campus.fct.unl.pt>
2022-11-19 09:55:03 +0000

    refac

--- src/main/java/business/auction/ListAuctionsAboutToCloseUseCase.java
+++ src/main/java/business/auction/ListAuctionsAboutToCloseUseCase.java
@@ -1,23 +1,24 @@
 package main.java.business.auction;
 
-import java.util.List;
 import com.azure.cosmos.util.CosmosPagedIterable;
+import main.java.DAL.cache.CachePlus;
 import main.java.DAL.gateway.IAuctionGateway;
 import main.java.DAL.repository.AuctionRepository;
-import main.java.DAL.cache.CachePlus;
 import main.java.models.DAO.AuctionDAO;
+
+import java.util.List;
 
 public class ListAuctionsAboutToCloseUseCase {
 
     public static CosmosPagedIterable<AuctionDAO> listAuctionsAboutToClose() {
-		IAuctionGateway auctionGateway = new AuctionRepository();
+        IAuctionGateway auctionGateway = new AuctionRepository();
         return auctionGateway.listAuctionsAboutToClose();
     }
-	
-	public static List<String> cacheListAuctionsAboutToClose() throws Exception {
-		List<String> res = CachePlus.cacheGet(CachePlus.AUCTION_LIST, null);
-		if(res==null)
-			throw new Exception("Not in cache");
-		return res;
+
+    public static List<String> cacheListAuctionsAboutToClose() throws Exception {
+        List<String> res = CachePlus.cacheGet(CachePlus.AUCTION_LIST, null);
+        if (res == null) throw new Exception("Not in cache");
+        else if (res.isEmpty()) throw new Exception("Not in cache");
+        else return res;
     }
 }
